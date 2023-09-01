de57bbc5e646ba598fc09c2d2e70a39c760be61c
Euvinni <marcosvdsc1212@gmail.com>
2023-08-31 21:40:19 -0300

    Adicionado mudanças

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dataBase_1 = require("./dataBase");
+console.log("Projeto rodando!");
+console.table(dataBase_1.users);
+console.table(dataBase_1.products);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {users, products} from './dataBase'
+
+console.log("Projeto rodando!");
+console.table(users);
+console.table(products);
+
